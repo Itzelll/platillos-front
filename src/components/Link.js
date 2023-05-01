0ca889d9a2b1181c4caeef7f53b5f5e8565cb17f
Itzell <mmitzel2435@gmail.com>
2023-04-30 19:42:32 -0600

    front css

--- src/components/Link.js
+++ src/components/Link.js
@@ -5,7 +5,36 @@ const Link = (props) => {
   return (
     <div>
       <div>
-        {link.nombre} ({link.tipoPlatillo}) ({link.calorias}) ({link.proteinas}) ({link.paisOrigen}) ({link.ingredientes}) ({link.saludable}) ({link.tiempoCoccion}) ({link.dificultadPreparacion}) ({link.utensiliosRequeridos})
+        <table class="content-table">
+          <thead>
+            <tr>
+              <th>Nombre</th>
+              <th>Tipo platillo</th>
+              <th>Calorias</th>
+              <th>Proteinas</th>
+              <th>Pais de origen</th>
+              <th>Ingredientes</th>
+              <th>Saludable</th>
+              <th>Tiempo coccion</th>
+              <th>Dificultad de preparacion</th>
+              <th>Utensilios requeridos</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>{link.nombre}</td>
+              <td>{link.tipoPlatillo}</td>
+              <td>{link.calorias}</td>
+              <td>{link.proteinas}</td>
+              <td>{link.paisOrigen}</td>
+              <td>{link.ingredientes}</td>
+              <td>{link.saludable}</td>
+              <td>{link.tiempoCoccion}</td>
+              <td>{link.dificultadPreparacion}</td>
+              <td>{link.utensiliosRequeridos}</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </div>
   );
